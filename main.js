5cbfd4766b60caccfe63044d63043de20302b500
polianarm <polianariveiro@gmail.com>
2023-03-13 17:36:42 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+/* a. O formulário deve ter pelo menos 4 campos: Título, Descrição e URL da imagem e um Botão de submit.
+b. Após o preenchimento, ao clicar no botão de submit, as informações devem ser validadas e adicionadas à lista de cards 
+na página.
+➔ Dica: Você pode acrescentar outros campos ao formulário para deixar a aplicação mais completa.
+
+5. Validações esperadas: 
+a. Nenhum campo do formulário pode ser nulo e/ou vazio.
+b. Os campos Título e Descrição devem possuir ao menos 4 caracteres.
+c. O botão de cadastro/submit só deve estar disponível/habilitado, 
+quando todas as validações tiverem sido realizadas corretamente.
+d. Um novo card só deve ser cadastrado, quando todas as validações tiverem sido realizadas corretamente.*/
+
+
