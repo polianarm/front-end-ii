e05efbe405af830898b4b82fcb31e892ce68e762
polianarm <polianariveiro@gmail.com>
2023-03-14 02:06:18 -0300

    novas alterações no form

--- main.js
+++ main.js
@@ -4,10 +4,39 @@ na página.
 ➔ Dica: Você pode acrescentar outros campos ao formulário para deixar a aplicação mais completa.
 
 5. Validações esperadas: 
-a. Nenhum campo do formulário pode ser nulo e/ou vazio.
-b. Os campos Título e Descrição devem possuir ao menos 4 caracteres.
+a. Nenhum campo do formulário pode ser nulo e/ou vazio. ok
+b. Os campos Título e Descrição devem possuir ao menos 4 caracteres. ok
 c. O botão de cadastro/submit só deve estar disponível/habilitado, 
 quando todas as validações tiverem sido realizadas corretamente.
 d. Um novo card só deve ser cadastrado, quando todas as validações tiverem sido realizadas corretamente.*/
 
 
+const inputTitleRef = document.querySelector('#title')
+const inputImageRef = document.querySelector('#image')
+var formErrors = {
+
+    title: true
+    image: true
+
+}
+
+function validateInput(inputRef) {
+
+    const inputValid = inputRef.checkValidity()
+    const div = inputRef.parentElement
+
+    if(inputValid) {
+
+        div.classList.remove('error')
+
+   } else {
+
+   div.classList.add('error')
+
+   }
+
+
+}
+
+
+inputEmailRef.addEventListener('keyup', validateEmail) //keyup> validar após a escrita 
